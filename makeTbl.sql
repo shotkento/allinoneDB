ef846a58687a74863c9b20feb0c27a66e51ce412
Kento <hide.zilch@gmail.com>
2014-09-15 22:37:27 +0900

    first commit

---
+++ makeTbl.sql
@@ -0,0 +1,7 @@
+create table allinone (
+	_id integer primary key not null,
+	day integer not null,
+	english_txt text not null,
+	japanese_txt_order text not null,
+	japanese_txt_normal text not null
+);
